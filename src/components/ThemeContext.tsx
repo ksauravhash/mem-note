57637682a9994e6ac19ea839ac4a03a90ea34ef1
Saurav Kumar <ksauravhash@gmail.com>
2025-01-25 15:52:37 +0530

    theme update

--- src/components/ThemeContext.tsx
+++ src/components/ThemeContext.tsx
@@ -3,8 +3,24 @@ import { ReactNode } from "react";
 
 const theme = createTheme({
   palette: {
-    mode: 'dark'
-  }
+    mode: "dark",
+  },
+  typography: {
+    fontFamily: '"Nunito", sans-serif',
+    h1: {
+      fontFamily: '"Lexend", sans-serif',
+      fontSize: "3rem", 
+      lineHeight: 1.2, 
+    },
+    h2: {
+      fontFamily: '"Lexend", sans-serif',
+      fontSize: "2rem",
+    },
+    body1: {
+      fontFamily: '"Nunito", sans-serif',
+      fontSize: "1rem",
+    },
+  },
 });
 
 const ThemeContext = ({ children }: { children: ReactNode }) => {
