2f461598fdb3a18d17b8ad2c4d1b54ece53f495f
Saurav Kumar <ksauravhash@gmail.com>
2025-01-26 16:46:10 +0530

    Dashboard

--- src/components/Dashboard.tsx
+++ src/components/Dashboard.tsx
@@ -1,7 +1,45 @@
-const Dashboard = () => {
-  return (
-    <div>Dashboard</div>
-  )
-}
+import { Box, Button, Container, Link, Stack, Typography } from "@mui/material";
+import { useContext, useEffect, useState } from "react";
+import axiosInstance from "../utility/axiosInstance";
+import { AuthContext } from "./Auth";
 
-export default Dashboard
+type NotebookData = {
+  title: string;
+  id: string;
+};
+
+const Dashboard = () => {
+  const [recentNotebooks, setRecentNotebooks] = useState<NotebookData[]>();
+  const authVauesOb = useContext(AuthContext);
+  const updateRecentNotebooks = async () => {
+    const notebooks = (await axiosInstance.get("/notebook/getRecentNotebooks", {
+      headers: {
+        Authorization: `bearer ${authVauesOb?.authValues?.accessToken}`,
+      },
+    })).data as NotebookData[];
+
+    setRecentNotebooks(notebooks);
+  };
+  useEffect(() => {
+    updateRecentNotebooks();
+  }, []);
+  return (
+    <Box margin={4}>
+      <Container sx={{ marginBottom: 4 }}>
+        <Typography variant="h1">Notebooks</Typography>
+      </Container>
+      <Container>
+        <Typography variant="h2" marginBottom={2}>
+          Recent
+        </Typography>
+        <Stack direction={"row"} spacing={2}>
+          {recentNotebooks && recentNotebooks.map((item) => (
+            <Link component={Button}>{item.title}</Link>
+          ))}
+        </Stack>
+      </Container>
+    </Box>
+  );
+};
+
+export default Dashboard;
